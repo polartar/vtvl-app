edfaa179744de458a0c9288b2310f84e416c563b
Ada Orajiaku <admin@vtvl.io>
2022-11-17 11:12:43 +0100

    fix for multiple onboarding issues

--- pages/api/email/login.ts
+++ pages/api/email/login.ts
@@ -12,14 +12,16 @@ type Data = {
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   const { email, newUser } = req.body;
   const baseUrl = process.env.NEXT_PUBLIC_DOMAIN_NAME;
+  const url = `${baseUrl}/${
+    newUser === true ? `onboarding/select-user-type?email=${email}&newUser=${newUser}` : `dashboard?email=${email}`
+  }`;
 
   const actionCodeSettings = {
-    url: `${baseUrl}/onboarding/select-user-type?email=${email}&newUser=${newUser}`,
+    url,
     handleCodeInApp: true
   };
 
   const emailLink = await firebaseAdmin?.auth().generateSignInWithEmailLink(email, actionCodeSettings);
-  console.log('sign in link generated is ', emailLink);
   await SendMail({
     to: email,
     data: { emailLink },
