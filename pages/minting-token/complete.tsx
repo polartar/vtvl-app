0e8be9660205fa0c94148eba5fa52b43aac175cb
Arvin Mito <admin@vtvl.io>
2022-11-20 23:51:45 +0800

    Fix #142 - multisig account check

--- pages/minting-token/complete.tsx
+++ pages/minting-token/complete.tsx
@@ -1,12 +1,20 @@
+import PageLoader from '@components/atoms/PageLoader/PageLoader';
 import SteppedLayout from '@components/organisms/Layout/SteppedLayout';
+import { useAuthContext } from '@providers/auth.context';
 import { useTokenContext } from '@providers/token.context';
+import { useWeb3React } from '@web3-react/core';
 import Router from 'next/router';
 import { NextPageWithLayout } from 'pages/_app';
 import ArrowIcon from 'public/icons/arrow-small-left.svg';
-import { ReactElement, useEffect } from 'react';
+import { ReactElement, useEffect, useState } from 'react';
+import { fetchSafeByQuery } from 'services/db/safe';
 
 const Complete: NextPageWithLayout = () => {
-  const { mintFormState } = useTokenContext();
+  const { account } = useWeb3React();
+  const { mintFormState, isTokenLoading } = useTokenContext();
+  const { safe } = useAuthContext();
+  const [isMultiSig, setIsMultiSig] = useState(false);
+  const [isPageLoading, setIsPageLoading] = useState(true);
 
   useEffect(() => {
     if (!mintFormState) {
@@ -14,33 +22,64 @@ const Complete: NextPageWithLayout = () => {
     }
   }, [mintFormState]);
 
+  // Set if using multisig
+  useEffect(() => {
+    setIsMultiSig(Boolean(safe?.address) && !account);
+  }, [safe, account]);
+
+  // Remove loader when token is fetched
+  useEffect(() => {
+    if (!isTokenLoading || !mintFormState.address) setIsPageLoading(false);
+  }, [isTokenLoading]);
+
   return (
-    <div className="panel rounded-lg mx-auto max-w-xl w-2/5 mt-14 text-center">
-      <h2 className="h4 text-neutral-900 mb-3">
-        Transaction is sent succesfully!
-        {/* <strong>{mintFormState?.symbol}</strong> logo: token created address: successfully! */}
-      </h2>
-      {/* <p className="text-gray-500 text-sm mb-9">
+    <>
+      {isPageLoading ? (
+        <PageLoader />
+      ) : (
+        <div className="panel rounded-lg mx-auto w-full max-w-lg mt-14 text-center">
+          <h2 className="h4 text-neutral-900 mb-3">
+            {isMultiSig ? (
+              'Transaction has been created'
+            ) : (
+              <>
+                <strong>{mintFormState?.symbol}</strong> token created successfully!
+              </>
+            )}
+          </h2>
+          {/* <p className="text-gray-500 text-sm mb-9">
         Your delegated employee will received a notification and can proceed to create the vesting schedules.
       </p> */}
-      {/* This next paragraph is displayed only when the user uses multi-sig to mint. */}
-      <p className="text-gray-500 text-sm mb-9">
-        The token creation is still in progress. In order to complete this step, 2 out of 3 owners is required to
-        confirm this transaction.
-      </p>
-      {mintFormState?.logo ? <img src={mintFormState.logo} className="w-22 h-22 mb-4 mx-auto" /> : null}
-      <h3 className="font-bold h4 uppercase mb-6">{mintFormState?.name}</h3>
-      <p className="text-sm text-neutral-500 mb-6">{mintFormState?.address}</p>
-      <div className="flex flex-row justify-between items-center border-t border-neutral-200 pt-5">
-        <a href="/dashboard" className="flex flex-row items-center gap-3 text-neutral-500">
-          Continue later
-          <ArrowIcon className="fill-current transform rotate-180" />
-        </a>
-        <button className="primary" type="button" onClick={() => Router.push('/dashboard')}>
-          Create vesting contract
-        </button>
-      </div>
-    </div>
+          {isMultiSig ? (
+            <p className="text-gray-500 text-sm mb-9">
+              The creation of token is in progress. In order for this process to be complete, 2 out of 3 owners are
+              required to execute this transaction.
+            </p>
+          ) : null}
+          {mintFormState?.logo ? (
+            <img src={mintFormState.logo} className="w-20 h-20 mb-4 mx-auto rounded-full" />
+          ) : null}
+          <h3 className="font-bold h4 uppercase mb-6">{mintFormState?.name}</h3>
+          <p className="text-sm text-neutral-500 mb-6">{mintFormState?.address}</p>
+          <div className="flex flex-row justify-between items-center border-t border-neutral-200 pt-5">
+            {!isMultiSig ? (
+              <button className="primary" type="button" onClick={() => Router.push('/vesting-schedule/configure')}>
+                Create schedule
+              </button>
+            ) : null}
+            <a href="/dashboard" className="flex flex-row items-center gap-3 text-neutral-500">
+              Continue later
+              <ArrowIcon className="fill-current transform rotate-180" />
+            </a>
+            {isMultiSig ? (
+              <button className="primary" type="button" onClick={() => Router.push('/dashboard')}>
+                Create vesting contract
+              </button>
+            ) : null}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
