e23d709cc4cbed3b9c1f49003cad889f3e6bae13
Arvin Mito <admin@vtvl.io>
2022-11-30 01:22:11 +0800

    Add dashboard link on manager role; Add BICO invite sending

--- pages/test.tsx
+++ pages/test.tsx
@@ -9,13 +9,19 @@ const TestPage: NextPage = () => {
   const handleSendInvite = async () => {
     // (email: string, type: string, userName: string, orgName: string, orgId?: string)
     setSending(true);
-    await sendTeammateInvite('nisha@vtvl.io', 'manager', 'Nisha', '1129test', 'nrHTFmzrofiHSlSqUdFH');
+    // PROD
+    await sendTeammateInvite('purva.gera@biconomy.io', 'manager', 'Purva', 'Biconomy', 'ggDbYxw5UtoeRNnUwPMm');
+    await sendTeammateInvite('ahmed@biconomy.io', 'manager', 'Ahmed', 'Biconomy', 'ggDbYxw5UtoeRNnUwPMm');
+    await sendTeammateInvite('arshita@biconomy.io', 'manager', 'Arshita', 'Biconomy', 'ggDbYxw5UtoeRNnUwPMm');
+    await sendTeammateInvite('aniket@biconomy.io', 'manager', 'Aniket', 'Biconomy', 'ggDbYxw5UtoeRNnUwPMm');
+    // QA TEST
+    // await sendTeammateInvite('nisha@vtvl.io', 'manager', 'Nisha', '1129test', 'nrHTFmzrofiHSlSqUdFH');
     setSending(false);
   };
   return (
     <div className="text-left p-10 w-full">
       <div className="flex flex-row items-center gap-2">
-        <h5>Send invite to Nisha</h5>
+        <h5>Send invite to BICO managers</h5>
         <Button className="secondary" loading={sending} onClick={handleSendInvite}>
           Invite
         </Button>
