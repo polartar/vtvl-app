863874af1dca13cc8f38e714c3af8784cd1e5442
VTVL-admin <admin@vtvl.io>
2023-02-02 09:59:59 -0800

    justify tabs center

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -30,25 +30,21 @@ const Settings = () => {
     <>
       <h1 className="h2 font-semibold text-center mb-10">{isTeamPage ? 'Team Management' : 'Gnosis Safe'}</h1>
 
-      <div className="w-full flex border-t-[1px] border-b-[1px] border-gray-200 mb-12">
-        <div className="w-[400px] h-1"></div>
+      <div className="w-full justify-center flex border-t-[1px] border-b-[1px] border-gray-200 mb-12 font-medium">
+        <div
+          className={`flex items-center h-14 mr-13 ${
+            isTeamPage ? 'text-gray-800  border-b-[#2b298b] border-b-2' : 'text-gray-400'
+          } cursor-pointer`}
+          onClick={() => !isTeamPage && handleChangeTab('team')}>
+          Team
+        </div>
 
-        <div className="pl-4 flex items-center font-medium">
-          <div
-            className={`flex items-center h-14 mr-13 ${
-              isTeamPage ? 'text-gray-800  border-b-[#2b298b] border-b-2' : 'text-gray-400'
-            } cursor-pointer`}
-            onClick={() => !isTeamPage && handleChangeTab('team')}>
-            Team
-          </div>
-
-          <div
-            className={`flex items-center h-14 ${
-              isTeamPage ? 'text-gray-400 ' : 'text-gray-800 border-b-[#2b298b] border-b-2'
-            } cursor-pointer`}
-            onClick={() => isTeamPage && handleChangeTab('safe')}>
-            Gnosis Safe
-          </div>
+        <div
+          className={`flex items-center h-14 ${
+            isTeamPage ? 'text-gray-400 ' : 'text-gray-800 border-b-[#2b298b] border-b-2'
+          } cursor-pointer`}
+          onClick={() => isTeamPage && handleChangeTab('safe')}>
+          Gnosis Safe
         </div>
       </div>
 
