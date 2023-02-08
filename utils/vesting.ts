52ef9266c519fdfb93766b4c2f55b81eee33af65
admin-VTVL <88101007+VTVL-admin@users.noreply.github.com>
2023-02-08 19:06:45 +0800

    Hotfix: Update the projected end date (#536)
    * Refactor release freq timestamp computation
    
    * Ensure actual dates on vesting schedule page
    
    * Optimize fix for continuous
    
    * Fix template options issue on new user
    
    * Subtract 1 second on the projected end date

--- utils/vesting.ts
+++ utils/vesting.ts
@@ -13,6 +13,7 @@ import format from 'date-fns/format';
 import formatDuration from 'date-fns/formatDuration';
 import getUnixTime from 'date-fns/getUnixTime';
 import intervalToDuration from 'date-fns/intervalToDuration';
+import sub from 'date-fns/sub';
 import Decimal from 'decimal.js';
 import { CliffDuration, DateDurationOptionsPlural, ReleaseFrequency } from 'types/constants/schedule-configuration';
 import { IChartDataTypes } from 'types/vesting';
@@ -481,6 +482,16 @@ export const getChartData = ({ start, end, cliffDuration, cliffAmount, frequency
       }
     }
   }
+
+  // Update projected end date and subtract 1 second
+  // Example:
+  // start date = January 1, 2023
+  // end date = January 1, 2027
+  // no cliff
+  // frequency = monthly
+  // projected end date and display should be = December 31, 2026
+  projectedEndDateTime = sub(projectedEndDateTime, { seconds: 1 });
+
   // Move the projected end date time here
   return { release: releaseData, cliff: cliffData, projectedEndDateTime };
 };
