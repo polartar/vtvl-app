a1f37ec3ff0350c6fe601359aa7a96f6c18f265a
admin-VTVL <88101007+VTVL-admin@users.noreply.github.com>
2023-02-23 10:43:53 +0800

    Update vest amounts computation (#591)

--- components/organisms/DashboardPendingActions/VestingSchedulePendingAction.tsx
+++ components/organisms/DashboardPendingActions/VestingSchedulePendingAction.tsx
@@ -457,9 +457,13 @@ const VestingSchedulePendingAction: React.FC<{ id: string; data: IVesting }> = (
       // const vestingLinearVestAmounts = new Array(vesting.recipients.length).fill(
       //   parseTokenAmount(vestingAmountPerUser, 18)
       // );
-      const vestingLinearVestAmounts = vesting.recipients.map((recipient) =>
-        parseTokenAmount(Number(recipient.allocations), 18)
-      );
+      const vestingLinearVestAmounts = vesting.recipients.map((recipient) => {
+        const { cliffDuration, lumpSumReleaseAfterCliff } = vesting.details;
+        // Computes how many tokens are left after cliff based on percentage
+        const percentage = 1 - (cliffDuration !== 'no-cliff' ? +lumpSumReleaseAfterCliff : 0) / 100;
+        return parseTokenAmount(Number(recipient.allocations) * percentage, 18);
+      });
+
       const vestingCliffAmounts = new Array(vesting.recipients.length).fill(parseTokenAmount(cliffAmountPerUser, 18));
 
       const CREATE_CLAIMS_BATCH_FUNCTION =
