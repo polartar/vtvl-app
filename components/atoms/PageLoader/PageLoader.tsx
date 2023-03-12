22d02ce975f8727fb1bd9e0e1623d50487f1e6c3
Arvin Mito <admin@vtvl.io>
2023-03-12 22:55:40 +0800

    Add global loader page

--- components/atoms/PageLoader/PageLoader.tsx
+++ components/atoms/PageLoader/PageLoader.tsx
@@ -1,11 +1,19 @@
 import Lottie from 'lottie-react';
 import VTVLLoaderData from 'public/VTVL_Loader.json';
 
-const PageLoader = () => {
+interface IPageLoaderProps {
+  loader?: 'default' | 'global';
+}
+
+const PageLoader = ({ loader = 'default' }: IPageLoaderProps) => {
   return (
     <div className="w-full flex items-start justify-center absolute top-0 bottom-0 left-0 right-0 z-20 bg-white pt-32">
       <div className="mx-auto text-center">
-        <Lottie animationData={VTVLLoaderData} style={{ width: 132, margin: 'auto' }} />
+        {loader === 'global' ? (
+          <img src="/global-loader.gif" className="h-32 w-32 mx-auto" />
+        ) : (
+          <Lottie animationData={VTVLLoaderData} style={{ width: 132, margin: 'auto' }} />
+        )}
         <p className="sora text-h1 font-semibold mb-4">Drum roll please...</p>
         <p className="text-sm text-neutral-500">
           We’re putting everything together.
