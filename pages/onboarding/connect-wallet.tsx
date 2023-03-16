10d3b49eb44785408ed8a0f084cf21750e6d996f
Arvin Mito <admin@vtvl.io>
2023-03-16 10:53:56 +0800

    Update carousel items + remove warnings

--- pages/onboarding/connect-wallet.tsx
+++ pages/onboarding/connect-wallet.tsx
@@ -38,33 +38,6 @@ const Vesting = styled.div<{ background?: string }>`
   background-size: cover;
 `;
 
-const carouselItems = [
-  {
-    title: ['100% ', <strong>no-code</strong>, <br />, 'ready in minutes'],
-    image: '/images/how-it-works/1.png',
-    subtitle: 'Mint or bring your own token',
-    description: 'Variable or fixed supply? No problem, you have options.'
-  },
-  {
-    title: ['Create multiple ', <strong>vesting smart contracts</strong>, ' in just a few clicks'],
-    image: '/images/how-it-works/2.png',
-    subtitle: 'Generate smart contracts for investors & employees',
-    description: 'We get it, have your engineers build YOUR product and let us take care of the custom vesting systems'
-  },
-  {
-    title: ['Automate ', <strong>custom token</strong>, ' distributions to your holders'],
-    image: '/images/how-it-works/3.png',
-    subtitle: 'Track your own tokens',
-    description: 'Say goodbye to managing via spreadsheet.'
-  },
-  {
-    title: ['Token vesting analytics ', <br />, <strong>coming soon!</strong>],
-    image: '/images/how-it-works/4.png',
-    subtitle: 'Token analytics coming soon',
-    description: 'What you really want to know about your tokenomics.'
-  }
-];
-
 const ConnectWalletPage: NextPage = () => {
   const { active, activate } = useWeb3React();
   const { onNext, startOnboarding, completeOnboarding } = useContext(OnboardingContext);
@@ -103,7 +76,7 @@ const ConnectWalletPage: NextPage = () => {
         <Vesting
           className="flex flex-col items-center justify-center pt-12 pb-10"
           background={assets?.loginBgImage?.src}>
-          <Carousel variant="dark" items={carouselItems} />
+          <Carousel variant="dark" />
         </Vesting>
       </OnboardingContainer>
     </PaddedLayout>
