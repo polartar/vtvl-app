30e6cd83163b9e784ac8bb93f6d3427018168f24
admin-VTVL <88101007+VTVL-admin@users.noreply.github.com>
2023-04-12 09:38:54 +0800

    refactor supported chains (#696)
    * refactor supported chains
    
    * add testnet

--- types/constants/supported-chains.ts
+++ types/constants/supported-chains.ts
@@ -24,24 +24,18 @@ export const PROD_SUPPORTED_CHAIN_IDS = [
   SupportedChainId.FANTOM,
   SupportedChainId.CRONOS,
   SupportedChainId.OKC_MAINNET,
-  SupportedChainId.OKC_TESTNET
+  SupportedChainId.GOERLI,
+  SupportedChainId.MUMBAI
 ];
 
 export const DEV_SUPPORTED_CHAIN_IDS = [
   SupportedChainId.GOERLI,
   SupportedChainId.MUMBAI,
   SupportedChainId.OKC_TESTNET,
-  SupportedChainId.BASE_GOERLI,
-  SupportedChainId.FANTOM_TESTNET,
-  SupportedChainId.CRONOS_TESTNET,
-  SupportedChainId.OKC_MAINNET,
-  SupportedChainId.OKC_TESTNET
+  SupportedChainId.BASE_GOERLI
 ];
 
 export const SUPPORTED_CHAIN_IDS = env === 'production' ? PROD_SUPPORTED_CHAIN_IDS : DEV_SUPPORTED_CHAIN_IDS;
-
-type ProdSupportedChainIds = typeof PROD_SUPPORTED_CHAIN_IDS[number];
-type DevSupportedChainIds = typeof DEV_SUPPORTED_CHAIN_IDS[number];
 
 interface Network {
   id: number;
@@ -53,17 +47,11 @@ interface Network {
   multisigTxUrl: string;
 }
 
-type ProdSupportedChainsType = {
-  [P in ProdSupportedChainIds]: Network;
+type ChainsType = {
+  [network: number]: Network;
 };
 
-type DevSupportedChainsType = {
-  [P in DevSupportedChainIds]: Network;
-};
-
-type SupportedChainsType = OnlyOne<ProdSupportedChainsType, DevSupportedChainsType>;
-
-export const prodSupportedChains: SupportedChainsType = {
+export const prodSupportedChains: ChainsType = {
   [SupportedChainId.MAINNET]: {
     id: 1,
     icon: '/icons/chains/ethereum.svg',
@@ -118,15 +106,7 @@ export const prodSupportedChains: SupportedChainsType = {
     explorer: 'https://cronos.crypto.org/explorer',
     multisigTxUrl: ''
   },
-  [SupportedChainId.MUMBAI]: {
-    id: 80001,
-    icon: '/icons/chains/polygon.svg',
-    title: 'Polygon Mumbai Testnet',
-    code: 'MATIC',
-    rpc: 'https://rpc-mumbai.maticvigil.com',
-    explorer: 'https://mumbai.polygonscan.com',
-    multisigTxUrl: 'https://safe-transaction-polygon.safe.global'
-  },
+
   [SupportedChainId.OKC_MAINNET]: {
     id: 66,
     icon: '/icons/chains/okc.png',
@@ -135,27 +115,6 @@ export const prodSupportedChains: SupportedChainsType = {
     rpc: 'https://exchainrpc.okex.org',
     explorer: 'https://www.oklink.com/en/okc',
     multisigTxUrl: ''
-  },
-  [SupportedChainId.OKC_TESTNET]: {
-    id: 65,
-    icon: '/icons/chains/okc.png',
-    title: 'OKC Testnet',
-    code: 'OKT',
-    rpc: 'https://exchaintestrpc.okex.org/',
-    explorer: 'https://www.oklink.com/en/okc-test',
-    multisigTxUrl: ''
-  }
-};
-
-export const devSupportedChains: SupportedChainsType = {
-  [SupportedChainId.MAINNET]: {
-    id: 5,
-    icon: '/icons/chains/ethereum.svg',
-    title: 'Mainnet-Test',
-    code: 'ETH',
-    rpc: 'https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
-    explorer: 'https://goerli.etherscan.io',
-    multisigTxUrl: 'https://safe-transaction-goerli.safe.global'
   },
   [SupportedChainId.GOERLI]: {
     id: 5,
@@ -166,15 +125,7 @@ export const devSupportedChains: SupportedChainsType = {
     explorer: 'https://goerli.etherscan.io',
     multisigTxUrl: 'https://safe-transaction-goerli.safe.global'
   },
-  [SupportedChainId.BINANCE]: {
-    id: 97,
-    icon: '/icons/chains/bsc.svg',
-    title: 'Binance Smart Chain Testnet',
-    code: 'BSC',
-    rpc: 'https://speedy-nodes-nyc.moralis.io/476f8ed27ca8c180ebc32f48/bsc/testnet',
-    explorer: 'https://testnet.bscscan.com',
-    multisigTxUrl: ''
-  },
+
   [SupportedChainId.MUMBAI]: {
     id: 80001,
     icon: '/icons/chains/polygon.svg',
@@ -183,51 +134,28 @@ export const devSupportedChains: SupportedChainsType = {
     rpc: 'https://rpc-mumbai.maticvigil.com',
     explorer: 'https://mumbai.polygonscan.com',
     multisigTxUrl: 'https://safe-transaction-polygon.safe.global'
+  }
+};
+
+export const devSupportedChains: ChainsType = {
+  [SupportedChainId.GOERLI]: {
+    id: 5,
+    icon: '/icons/chains/ethereum.svg',
+    title: 'Goerli',
+    code: 'ETH',
+    rpc: 'https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+    explorer: 'https://goerli.etherscan.io',
+    multisigTxUrl: 'https://safe-transaction-goerli.safe.global'
   },
-  [SupportedChainId.POLYGON]: {
-    id: 137,
+
+  [SupportedChainId.MUMBAI]: {
+    id: 80001,
     icon: '/icons/chains/polygon.svg',
-    title: 'Polygon',
+    title: 'Polygon Mumbai Testnet',
     code: 'MATIC',
-    rpc: 'https://polygon-rpc.com',
-    explorer: 'https://polygonscan.com/',
+    rpc: 'https://rpc-mumbai.maticvigil.com',
+    explorer: 'https://mumbai.polygonscan.com',
     multisigTxUrl: 'https://safe-transaction-polygon.safe.global'
-  },
-  [SupportedChainId.AVALANCHE]: {
-    id: 43113,
-    icon: '/icons/chains/avalanche.svg',
-    title: 'Avalanche Testnet',
-    code: 'AVAX',
-    rpc: 'https://api.avax-test.network/ext/bc/C/rpc',
-    explorer: 'https://snowtrace.io/',
-    multisigTxUrl: ''
-  },
-  [SupportedChainId.FANTOM]: {
-    id: 4002,
-    icon: '/icons/chains/fantom.svg',
-    title: 'Fantom Testnet',
-    code: 'FTM',
-    rpc: 'https://rpc.testnet.fantom.network/',
-    explorer: 'https://ftmscan.com/',
-    multisigTxUrl: ''
-  },
-  [SupportedChainId.CRONOS]: {
-    id: 338,
-    icon: '/icons/chains/cronos.svg',
-    title: 'Cronos Testnet',
-    code: 'CRO',
-    rpc: 'https://cronos-testnet-3.crypto.org:8545/',
-    explorer: 'https://cronos.crypto.org/explorer/testnet3/',
-    multisigTxUrl: ''
-  },
-  [SupportedChainId.OKC_MAINNET]: {
-    id: 66,
-    icon: '/icons/chains/okc.png',
-    title: 'OKC Mainnet',
-    code: 'OKT',
-    rpc: 'https://exchainrpc.okex.org',
-    explorer: 'https://www.oklink.com/en/okc',
-    multisigTxUrl: ''
   },
   [SupportedChainId.OKC_TESTNET]: {
     id: 65,
