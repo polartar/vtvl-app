9115e44aae3bdb6917cc4c15fc86decd5d890801
VTVL-admin <admin@vtvl.io>
2023-04-14 08:54:34 -0700

    fix style issue

--- components/organisms/VestingSchedule/VestingSummaryTable.tsx
+++ components/organisms/VestingSchedule/VestingSummaryTable.tsx
@@ -5,17 +5,20 @@ import SummaryRecipientRow from './SummaryRecipientRow';
 const VestingSummaryTable = ({ recipients }: { recipients: IRecipientDoc[] }) => {
   return (
     <div>
-      <div className="flex text-[#475467] text-xs w-full border-2">
-        <div className=" w-36 p-3 flex-shrink-0 bg-[#f2f4f7]">Name</div>
-        <div className="w-36 p-3 flex-shrink-0 bg-[#f2f4f7]">Status</div>
-        <div className="w-36 p-3 flex-shrink-0 bg-[#f2f4f7]">Updated On</div>
-        <div className=" w-full p-3   bg-[#f2f4f7]">Confirmation</div>
+      {/* <div className="flex text-[#475467] text-xs w-full border-2"> */}
+      <div className="border border-[#d0d5dd] border-b-0 rounded-xl w-full overflow-x-scroll mt-3">
+        <div className="flex text-[#475467] text-xs">
+          <div className=" w-36 p-3 flex-shrink-0 bg-[#f2f4f7]">Name</div>
+          <div className="w-36 p-3 flex-shrink-0 bg-[#f2f4f7]">Status</div>
+          <div className="w-36 p-3 flex-shrink-0 bg-[#f2f4f7]">Updated On</div>
+          <div className=" w-full p-3   bg-[#f2f4f7]">Confirmation</div>
+        </div>
+        {recipients
+          ?.filter((recipient) => recipient.data.status === 'accepted' && !!recipient.data.walletAddress)
+          .map((recipient) => {
+            return <SummaryRecipientRow recipient={recipient} key={recipient.id} />;
+          })}
       </div>
-      {recipients
-        ?.filter((recipient) => recipient.data.status === 'accepted' && !!recipient.data.walletAddress)
-        .map((recipient) => {
-          return <SummaryRecipientRow recipient={recipient} key={recipient.id} />;
-        })}
     </div>
   );
 };
