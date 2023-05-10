81261d2dbaa62e5f69c9262178d56814f649b825
VTVL-admin <admin@vtvl.io>
2023-05-09 18:28:30 -0700

    remove revocation notication in contracts page

--- components/organisms/VestingContracts/VestingContracts.tsx
+++ components/organisms/VestingContracts/VestingContracts.tsx
@@ -1,16 +1,8 @@
 import Copy from '@components/atoms/Copy/Copy';
 import { Typography } from '@components/atoms/Typography/Typography';
-import Safe from '@gnosis.pm/safe-core-sdk';
-import EthersAdapter from '@gnosis.pm/safe-ethers-lib';
-import SafeServiceClient from '@gnosis.pm/safe-service-client';
-import { useAuthContext } from '@providers/auth.context';
 import { useDashboardContext } from '@providers/dashboard.context';
 import { useTokenContext } from '@providers/token.context';
-import { useTransactionLoaderContext } from '@providers/transaction-loader.context';
-import { useWeb3React } from '@web3-react/core';
-import VestingABI from 'contracts/abi/VtvlVesting.json';
 import { BigNumber } from 'ethers';
-import { ethers } from 'ethers';
 import { formatEther } from 'ethers/lib/utils';
 import useChainVestingContracts from 'hooks/useChainVestingContracts';
 import { useModal } from 'hooks/useModal';
@@ -19,16 +11,6 @@ import { useRouter } from 'next/router';
 import PlusIcon from 'public/icons/plus.svg';
 import React, { useCallback } from 'react';
 import { useMemo } from 'react';
-import { useEffect, useState } from 'react';
-import { toast } from 'react-toastify';
-import { updateRecipient } from 'services/db/recipient';
-import { fetchRevokingsByQuery } from 'services/db/revoking';
-import { createTransaction, updateTransaction } from 'services/db/transaction';
-import { SupportedChainId, SupportedChains } from 'types/constants/supported-chains';
-import { ITransaction } from 'types/models';
-import { IRevokingDoc } from 'types/models/revoking';
-import { IVestingDoc } from 'types/models/vesting';
-import { IVestingContractDoc } from 'types/models/vestingContract';
 import { compareAddresses } from 'utils';
 
 import VestingContractCard from '../Cards/VestingContractCard';
@@ -37,63 +19,15 @@ import ContractsProfile from './VestingContractsProfile';
 
 export default function VestingContracts() {
   const { vestingContracts, vestings: allVestings, recipients: allRecipients } = useDashboardContext();
-  const { organizationId, currentSafe } = useAuthContext();
-  const { chainId, account, library } = useWeb3React();
   const { mintFormState: token } = useTokenContext();
   const { ModalWrapper, showModal, hideModal } = useModal({});
-  const [revokings, setRevokings] = useState<IRevokingDoc[]>();
   const router = useRouter();
-  const { mintFormState } = useTokenContext();
-  const [revocableContracts, setRevocableContracts] = useState<IVestingContractDoc[]>([]);
-  const { setTransactionStatus: setTransactionLoaderStatus } = useTransactionLoaderContext();
-  const [currentRevokingContract, setCurrentRevokingContract] = useState<string>();
-
-  useEffect(() => {
-    if (chainId && organizationId) {
-      fetchRevokingsByQuery(
-        ['chainId', 'organizationId', 'status'],
-        ['==', '==', '=='],
-        [chainId, organizationId, 'SUCCESS']
-      ).then((res) => {
-        if (res) {
-          setRevokings(res);
-        }
-      });
-    }
-  }, [chainId, organizationId]);
 
   const { vestingSchedules: vestingSchedulesInfo } = useChainVestingContracts(
     vestingContracts,
     allVestings,
     allRecipients
   );
-
-  useEffect(() => {
-    if (revokings && allVestings && allRecipients) {
-      const availableRevokings = revokings.filter(
-        (revoking) =>
-          Number(
-            allRecipients.find(
-              (recipient) =>
-                recipient.data.vestingId === revoking.data.vestingId &&
-                recipient.data.walletAddress === revoking.data.recipient
-            )?.data.allocations
-          ) !== 0
-      );
-      const uniqueVestingContracts: IVestingContractDoc[] = [];
-
-      availableRevokings.forEach((revoke) => {
-        const vesting = allVestings.find((vesting) => vesting.id === revoke.data.vestingId);
-        console.log(vesting?.id);
-        if (!uniqueVestingContracts.map((contract) => contract.id).includes(vesting?.data.vestingContractId || '')) {
-          const vestingContract = vestingContracts.find((contract) => contract.id === vesting?.data.vestingContractId);
-          if (vestingContract) uniqueVestingContracts.push(vestingContract);
-        }
-      }, []);
-
-      setRevocableContracts(uniqueVestingContracts);
-    }
-  }, [revokings, allRecipients, allVestings]);
 
   const getVestingInfoByContract = useCallback(
     (contractAddress: string) => {
@@ -132,146 +66,9 @@ export default function VestingContracts() {
     return vestingContracts.map((vestingContract) => getVestingInfoByContract(vestingContract.data.address));
   }, [vestingSchedulesInfo, getVestingInfoByContract, vestingContracts]);
 
-  const initRecipientAllocation = (vestingContractAddress: string) => {
-    const vestingContract = vestingContracts.find((contract) => contract.data.address === vestingContractAddress);
-    if (!vestingContract) return;
-    const vestingIds = allVestings
-      .filter((vesting) => vesting.data.vestingContractId === vestingContract.id)
-      .map((vesting) => vesting.id);
-    const availableRecipients = allRecipients.filter((recipient) => vestingIds.includes(recipient.data.vestingId));
-
-    availableRecipients.forEach((recipient) => {
-      updateRecipient(recipient.id, {
-        allocations: 0
-      });
-    });
-  };
-
-  const handleTransfer = async (vestingContractAddress: string) => {
-    if (vestingContracts && vestingContracts.length > 0 && organizationId && chainId && account) {
-      setCurrentRevokingContract(vestingContractAddress);
-
-      const vestingContract = new ethers.Contract(vestingContractAddress, VestingABI.abi, library.getSigner());
-      const vestingContractInfo = vestingContractsInfo.find((contract) => contract.address === vestingContractAddress);
-      try {
-        if (currentSafe?.address) {
-          const ADMIN_WITHDRAW_FUNCTION = 'function withdrawAdmin(uint112 _amountRequested)';
-          const ABI = [ADMIN_WITHDRAW_FUNCTION];
-          const vestingContractInterface = new ethers.utils.Interface(ABI);
-          const adminWithdrawEncoded = vestingContractInterface.encodeFunctionData('withdrawAdmin', [
-            vestingContractInfo?.reserved
-          ]);
-          const ethAdapter = new EthersAdapter({
-            ethers: ethers,
-            signer: library?.getSigner(0)
-          });
-
-          const safeSdk: Safe = await Safe.create({ ethAdapter: ethAdapter, safeAddress: currentSafe?.address });
-          const txData = {
-            to: vestingContractAddress,
-            data: adminWithdrawEncoded,
-            value: '0'
-          };
-          const safeTransaction = await safeSdk.createTransaction({ safeTransactionData: txData });
-          const txHash = await safeSdk.getTransactionHash(safeTransaction);
-          const signature = await safeSdk.signTransactionHash(txHash);
-          setTransactionLoaderStatus('IN_PROGRESS');
-          safeTransaction.addSignature(signature);
-          const safeService = new SafeServiceClient({
-            txServiceUrl: SupportedChains[chainId as SupportedChainId].multisigTxUrl,
-            ethAdapter
-          });
-          await safeService.proposeTransaction({
-            safeAddress: currentSafe.address,
-            senderAddress: account,
-            safeTransactionData: safeTransaction.data,
-            safeTxHash: txHash,
-            senderSignature: signature.data
-          });
-          const transactionData: ITransaction = {
-            hash: '',
-            safeHash: txHash,
-            status: 'PENDING',
-            to: vestingContractAddress,
-            type: 'ADMIN_WITHDRAW',
-            createdAt: Math.floor(new Date().getTime() / 1000),
-            updatedAt: Math.floor(new Date().getTime() / 1000),
-            organizationId: organizationId,
-            chainId,
-            vestingIds: [],
-            withdrawAmount: ethers.utils.formatUnits(vestingContractInfo!.reserved, mintFormState?.decimals || 18),
-            vestingContractId: vestingContracts[0].id
-          };
-          const transactionId = await createTransaction(transactionData);
-          toast.success('Transaction has been created successfully.');
-          setTransactionLoaderStatus('SUCCESS');
-        } else {
-          const withdrawTransaction = await vestingContract.withdrawAdmin(vestingContractInfo?.reserved);
-          const transactionData: ITransaction = {
-            hash: withdrawTransaction.hash,
-            safeHash: '',
-            status: 'PENDING',
-            to: vestingContractAddress,
-            type: 'ADMIN_WITHDRAW',
-            createdAt: Math.floor(new Date().getTime() / 1000),
-            updatedAt: Math.floor(new Date().getTime() / 1000),
-            organizationId: organizationId,
-            chainId,
-            vestingIds: [],
-            withdrawAmount: ethers.utils.formatUnits(vestingContractInfo!.reserved, mintFormState?.decimals || 18),
-            vestingContractId: vestingContracts[0].id
-          };
-          const transactionId = await createTransaction(transactionData);
-          await withdrawTransaction.wait();
-          await updateTransaction(
-            {
-              ...transactionData,
-              status: 'SUCCESS',
-              updatedAt: Math.floor(new Date().getTime() / 1000)
-            },
-            transactionId
-          );
-          toast.success('Withdrew tokens successfully.');
-          setTransactionLoaderStatus('SUCCESS');
-        }
-        const updatedRevocableContracts = revocableContracts.filter(
-          (contract) => !compareAddresses(contract.data.address, vestingContractAddress)
-        );
-        setRevocableContracts(updatedRevocableContracts.slice());
-        initRecipientAllocation(vestingContractAddress);
-      } catch (err) {
-        toast.error('Something went wrong');
-      } finally {
-        setCurrentRevokingContract(undefined);
-      }
-    }
-  };
-
   return (
     <div className="w-full">
       <div className="mb-9">
-        {/* {revocableContracts.map((vestingContract) => {
-          return (
-            <div
-              key={vestingContract.id}
-              className="mb-3 w-full px-6 py-3 bg-warning-100 border border-warning-500 rounded-lg flex items-center justify-between">
-              <div>
-                <div className="font-bold text-sm text-[#344054]">Unallocated tokens</div>
-                <div className="text-label text-sm">
-                  You can now transfer the locked tokens from revoke schedule under <b>{vestingContract.data.name}'s</b>{' '}
-                  contract and transfer them back to your wallet.
-                </div>
-              </div>
-              <button
-                className="secondary small whitespace-nowrap bg-gray-200 text-gray-500"
-                disabled={currentRevokingContract === vestingContract.data.address}
-                onClick={() => handleTransfer(vestingContract.data.address)}>
-                Transfer Tokens
-              </button>
-            </div>
-          );
-        })} */}
-
         <div className="flex justify-between">
           <Typography size="title" variant="inter" className=" font-semibold text-neutral-900 ">
             Contracts
