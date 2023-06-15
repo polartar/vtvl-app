83d2b33a41debed28d33848445637b0b5d052543
danny-vtvl <admin@vtvl.io>
2023-06-15 19:47:32 -0400

    update factory address

--- utils/constants.ts
+++ utils/constants.ts
@@ -36,5 +36,5 @@ export const WEBSITE_EMAIL = 'no-reply@vtvl.io';
 
 export const FACTORY_CONTRACTS: { [key: number]: string } = {
   5: '0x48CC6FFb03B14Cbf6315e99c8bfa42642848d7AA',
-  12009: '0x41d3f5e5744df065B34bc30be2021DDf8Ff72480'
+  12009: '0xF4c7E49ebc9B6Cb38711BA977Ca9478ba1d59dA1'
 };
