6c78e0fd902f05c9404d46d54107295d3ee68803
Sara Tavares <admin@vtvl.io>
2023-06-21 15:12:03 +0100

    add fix ci

--- cypress/e2e/sign-up-magic-link.cy.js
+++ cypress/e2e/sign-up-magic-link.cy.js
@@ -1,5 +1,5 @@
 import 'cypress-iframe';
-import faker from "@faker-js/faker";
+import faker from 'faker-js';
 
 const randomName = faker.person.fullName();
 const randomCompanyName = faker.lorem.word();
