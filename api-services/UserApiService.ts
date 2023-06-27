9e2f5f3ec089307823f90c84f49e0d1b4d6fce36
andy-vtvl <admin@vtvl.io>
2023-06-26 20:59:02 -0500

    Fix imports

--- api-services/UserApiService.ts
+++ api-services/UserApiService.ts
@@ -1,5 +1,4 @@
 import CoreApiService from '@api-services/CoreApiService';
-import { PLATFORM_NAME } from '@utils/constants';
 
 class UserApiService {
   getProfile = () => CoreApiService.get<string>('/auth/me');
