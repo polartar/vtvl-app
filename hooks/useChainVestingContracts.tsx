64d50242fa4b875187325ed61492a232820fcbe0
Arvin Mito <admin@vtvl.io>
2023-11-08 15:20:21 +0800

    Temporary fix: Optimize on chain calls

--- hooks/useChainVestingContracts.tsx
+++ hooks/useChainVestingContracts.tsx
@@ -42,17 +42,17 @@ export default function useChainVestingContracts(
   });
 
   useEffect(() => {
-    const arrVestingContracts = Array.isArray(vestingContracts) ? vestingContracts : [vestingContracts];
-    if (!chainId || !arrVestingContracts?.length) return;
+    const debounce = setTimeout(() => {
+      const arrVestingContracts = Array.isArray(vestingContracts) ? vestingContracts : [vestingContracts];
+      const filteredContracts = arrVestingContracts.filter((contract) => !!contract.address);
+      if (!chainId || !filteredContracts?.length) return;
 
-    setState({ isLoading: true });
-    const multicall = new Multicall({
-      ethersProvider: ethers.getDefaultProvider(SupportedChains[chainId as SupportedChainId].rpc),
-      tryAggregate: true
-    });
-    const contractCallContext: ContractCallContext[] = arrVestingContracts
-      .filter((contract) => !!contract.address)
-      .reduce((res, vestingContract) => {
+      const multicall = new Multicall({
+        ethersProvider: ethers.getDefaultProvider(SupportedChains[chainId as SupportedChainId].rpc),
+        tryAggregate: true
+      });
+
+      const contractCallContext: ContractCallContext[] = filteredContracts.reduce((res, vestingContract) => {
         let result: ContractCallContext[] = [];
         if (vestingContract.address) {
           const partialVestings = vestings
@@ -91,8 +91,11 @@ export default function useChainVestingContracts(
                               methodName: 'claimableAmount',
                               methodParameters: [recipient.address, 0]
                             },
-
-                            { reference: 'getClaim', methodName: 'getClaim', methodParameters: [recipient.address, 0] }
+                            {
+                              reference: 'getClaim',
+                              methodName: 'getClaim',
+                              methodParameters: [recipient.address, 0]
+                            }
                           ]
                         : [
                             {
@@ -112,96 +115,100 @@ export default function useChainVestingContracts(
         return [...res, ...result];
       }, [] as ContractCallContext[]);
 
-    multicall
-      .call(contractCallContext)
-      .then((response) => {
-        // Set constants for referencing the calls based on the multicall setup above
-        const WITHDRAWN_CALL = 1;
-        const CLAIMABLE_AMOUNT_CALL = 0;
+      multicall
+        .call(contractCallContext)
+        .then((response) => {
+          // Set constants for referencing the calls based on the multicall setup above
+          const WITHDRAWN_CALL = 1;
+          const CLAIMABLE_AMOUNT_CALL = 0;
 
-        const chainData: Array<{
-          address: string;
-          recipient: string;
-          unclaimed: BigNumber;
-          allocation: BigNumber;
-          withdrawn: BigNumber;
-          numTokensReservedForVesting: BigNumber;
-          locked: BigNumber;
-        }> = [];
-        Object.keys(response.results).forEach((key) => {
-          const value = response.results[key];
-          const fields = key.split('-');
-          const address = fields[1];
-          const reference = fields[0];
-          const recipient = fields[2];
+          const chainData: Array<{
+            address: string;
+            recipient: string;
+            unclaimed: BigNumber;
+            allocation: BigNumber;
+            withdrawn: BigNumber;
+            numTokensReservedForVesting: BigNumber;
+            locked: BigNumber;
+          }> = [];
+          Object.keys(response.results).forEach((key) => {
+            const value = response.results[key];
+            const fields = key.split('-');
+            const address = fields[1];
+            const reference = fields[0];
+            const recipient = fields[2];
 
-          const index = chainData.findIndex(
-            (data) => compareAddresses(data.address, address) && compareAddresses(data.recipient, recipient)
-          );
+            const index = chainData.findIndex(
+              (data) => compareAddresses(data.address, address) && compareAddresses(data.recipient, recipient)
+            );
 
-          const data =
-            index > -1
-              ? chainData[index]
-              : {
-                  allocation: BigNumber.from(0),
-                  withdrawn: BigNumber.from(0),
-                  unclaimed: BigNumber.from(0),
-                  locked: BigNumber.from(0),
-                  numTokensReservedForVesting: BigNumber.from(0),
-                  recipient
-                };
+            const data =
+              index > -1
+                ? chainData[index]
+                : {
+                    allocation: BigNumber.from(0),
+                    withdrawn: BigNumber.from(0),
+                    unclaimed: BigNumber.from(0),
+                    locked: BigNumber.from(0),
+                    numTokensReservedForVesting: BigNumber.from(0),
+                    recipient
+                  };
 
-          if (reference === 'numTokensReservedForVesting') {
-            data.numTokensReservedForVesting = BigNumber.from(value.callsReturnContext[0].returnValues[0]);
-          } else {
-            const record = value.callsReturnContext;
-            // Gets the claimable amount of the recipient
-            const claimableAmount = record[CLAIMABLE_AMOUNT_CALL].returnValues[0];
-            // Gets the total allocation of the recipient
-            // Gets the vested amount of the recipient -- which is the claimed and unclaimed tokens
-            // Computes the actual withdrawn amount by getting the claimed tokens
-            // unclaimed = claimableAmount
-            // claimed = vested amount - unclaimed
-            const claimedAmount = record[WITHDRAWN_CALL].returnValues[5];
-            const linearAmount = record[WITHDRAWN_CALL].returnValues[4];
-            const cliffAmount = record[WITHDRAWN_CALL].returnValues[6];
-            const totalAllocation = BigNumber.from(linearAmount).add(cliffAmount);
+            if (reference === 'numTokensReservedForVesting') {
+              data.numTokensReservedForVesting = BigNumber.from(value.callsReturnContext[0].returnValues[0]);
+            } else {
+              const record = value.callsReturnContext;
+              // Gets the claimable amount of the recipient
+              const claimableAmount = record[CLAIMABLE_AMOUNT_CALL].returnValues[0];
+              // Gets the total allocation of the recipient
+              // Gets the vested amount of the recipient -- which is the claimed and unclaimed tokens
+              // Computes the actual withdrawn amount by getting the claimed tokens
+              // unclaimed = claimableAmount
+              // claimed = vested amount - unclaimed
+              const claimedAmount = record[WITHDRAWN_CALL].returnValues[5];
+              const linearAmount = record[WITHDRAWN_CALL].returnValues[4];
+              const cliffAmount = record[WITHDRAWN_CALL].returnValues[6];
+              const totalAllocation = BigNumber.from(linearAmount).add(cliffAmount);
 
-            // Computes the locked tokens of the recipient
-            const lockedTokens = totalAllocation.sub(claimedAmount).sub(claimableAmount);
-            data.allocation = totalAllocation;
-            data.withdrawn = BigNumber.from(claimedAmount);
-            data.unclaimed = BigNumber.from(claimableAmount);
-            data.locked = BigNumber.from(lockedTokens);
-          }
+              // Computes the locked tokens of the recipient
+              const lockedTokens = totalAllocation.sub(claimedAmount).sub(claimableAmount);
+              data.allocation = totalAllocation;
+              data.withdrawn = BigNumber.from(claimedAmount);
+              data.unclaimed = BigNumber.from(claimableAmount);
+              data.locked = BigNumber.from(lockedTokens);
+            }
 
-          if (index > -1) {
-            chainData[index] = { address, ...data };
-          } else {
-            chainData.push({ address, ...data });
-          }
+            if (index > -1) {
+              chainData[index] = { address, ...data };
+            } else {
+              chainData.push({ address, ...data });
+            }
+          });
+
+          setState({
+            vestingSchedules: chainData.map((data) => {
+              // const locked = BigNumber.from(data.allocation)
+              //   .sub(BigNumber.from(data.withdrawn))
+              //   .sub(BigNumber.from(data.unclaimed));
+              return {
+                address: data.address,
+                recipient: data.recipient,
+                allocation: data.allocation,
+                withdrawn: data.withdrawn,
+                unclaimed: data.unclaimed,
+                numTokensReservedForVesting: data.numTokensReservedForVesting,
+                locked: data.locked.gte(0) ? data.locked : BigNumber.from(0)
+              };
+            })
+          });
+        })
+        .catch(console.error)
+        .finally(() => {
+          setState({ isLoading: false });
         });
-        setState({
-          vestingSchedules: chainData.map((data) => {
-            // const locked = BigNumber.from(data.allocation)
-            //   .sub(BigNumber.from(data.withdrawn))
-            //   .sub(BigNumber.from(data.unclaimed));
-            return {
-              address: data.address,
-              recipient: data.recipient,
-              allocation: data.allocation,
-              withdrawn: data.withdrawn,
-              unclaimed: data.unclaimed,
-              numTokensReservedForVesting: data.numTokensReservedForVesting,
-              locked: data.locked.gte(0) ? data.locked : BigNumber.from(0)
-            };
-          })
-        });
-      })
-      .catch(console.error)
-      .finally(() => {
-        setState({ isLoading: false });
-      });
+    }, 600);
+
+    return () => clearTimeout(debounce);
   }, [chainId, vestingContracts, recipients]);
 
   return { ...state };
